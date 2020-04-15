36fa89883ead60dc25ccd3dbf6100faaf7b2a495
Jack Wilkin <wilkin.j@husky.neu.edu>
2020-04-15 19:15:56 -0400

    Updates

---
+++ precache-manifest.722831638c16560e7a0ccc4de3b4dea3.js
@@ -0,0 +1,414 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da168330757c64bff1564aa152f4962f",
+    "url": "/vidnet/index.html"
+  },
+  {
+    "revision": "184ac27db5683dcdd5a2",
+    "url": "/vidnet/static/css/2.1af7a3c5.chunk.css"
+  },
+  {
+    "revision": "d360a3856c336a5f102f",
+    "url": "/vidnet/static/css/main.b458ab9c.chunk.css"
+  },
+  {
+    "revision": "184ac27db5683dcdd5a2",
+    "url": "/vidnet/static/js/2.f3969675.chunk.js"
+  },
+  {
+    "revision": "d360a3856c336a5f102f",
+    "url": "/vidnet/static/js/main.a7d4a9b6.chunk.js"
+  },
+  {
+    "revision": "e3d34b92d71ce65ed497",
+    "url": "/vidnet/static/js/runtime-main.5fbf5c78.js"
+  },
+  {
+    "revision": "536937c439f975794eb467ce4d8afc6d",
+    "url": "/vidnet/static/media/2001-Graduation.536937c4.mov"
+  },
+  {
+    "revision": "e5519863dcf7476d04049c89482e870e",
+    "url": "/vidnet/static/media/Berkeley-Rally-9_11.e5519863.mp4"
+  },
+  {
+    "revision": "b7dbaa7ed8592c9a0f90b49400329e3a",
+    "url": "/vidnet/static/media/Tijuana.b7dbaa7e.mov"
+  },
+  {
+    "revision": "05150d95867b7c7d5b45c672f93399ad",
+    "url": "/vidnet/static/media/academy-sports.05150d95.mov"
+  },
+  {
+    "revision": "58bd96d042e23e9f55519c8d79efcc5c",
+    "url": "/vidnet/static/media/animals.58bd96d0.mov"
+  },
+  {
+    "revision": "6847afe7605f665ef7dc03239fcfaa50",
+    "url": "/vidnet/static/media/aquarium-scenes.6847afe7.mov"
+  },
+  {
+    "revision": "d998607a2ac6dcc673c1aba6fc1a1b67",
+    "url": "/vidnet/static/media/aquarium.d998607a.mov"
+  },
+  {
+    "revision": "e6969089d7def930264b4a2e57f84704",
+    "url": "/vidnet/static/media/baseball-autograph.e6969089.mov"
+  },
+  {
+    "revision": "22c99973d5c1d055d05928e03cfc78cd",
+    "url": "/vidnet/static/media/baseball-kids.22c99973.mov"
+  },
+  {
+    "revision": "803485c5786d7f62a8b287c83733c302",
+    "url": "/vidnet/static/media/bmx-intro.803485c5.mov"
+  },
+  {
+    "revision": "92eae31b9168874fef39a37ceeb32979",
+    "url": "/vidnet/static/media/bmx-skatepark.92eae31b.mov"
+  },
+  {
+    "revision": "8520959b459c4c07898ca554a1516f2e",
+    "url": "/vidnet/static/media/bmx-woods.8520959b.mov"
+  },
+  {
+    "revision": "6781d40a20fc8a0d7e45d6106d06c2a8",
+    "url": "/vidnet/static/media/boat.6781d40a.mov"
+  },
+  {
+    "revision": "ebc27f11221387c547919d34bc45aa7a",
+    "url": "/vidnet/static/media/box-stacking.ebc27f11.mov"
+  },
+  {
+    "revision": "92fb380ac832d3b35d73a7ae1bb3417b",
+    "url": "/vidnet/static/media/boys-tackle.92fb380a.mov"
+  },
+  {
+    "revision": "b39316c4947e2c578233c69c350595de",
+    "url": "/vidnet/static/media/chalkboard-art.b39316c4.mov"
+  },
+  {
+    "revision": "0e54e28f2740d78a5b2cc8cbf09b3fc2",
+    "url": "/vidnet/static/media/cloudy.0e54e28f.mov"
+  },
+  {
+    "revision": "b9bde92a6cb337cd131aa983bf82eee6",
+    "url": "/vidnet/static/media/clubhouse-game.b9bde92a.mov"
+  },
+  {
+    "revision": "8706668879372452f5c0241703b4c9e6",
+    "url": "/vidnet/static/media/clubhouse-kids.87066688.mov"
+  },
+  {
+    "revision": "2f81259f5fc6e90b038663bd721a90e2",
+    "url": "/vidnet/static/media/day.2f81259f.mov"
+  },
+  {
+    "revision": "68d9550fc8b77fce23cc5afbb29c9cd7",
+    "url": "/vidnet/static/media/earth-gonna-die.68d9550f.mov"
+  },
+  {
+    "revision": "97490e6371ca1b165b5ebc843b3f1e49",
+    "url": "/vidnet/static/media/fair.97490e63.mov"
+  },
+  {
+    "revision": "0a542519b6682bc341beff10f46bc061",
+    "url": "/vidnet/static/media/fieldtrip1.0a542519.mov"
+  },
+  {
+    "revision": "b8802adc959900743d10dc4ac1a2bbaa",
+    "url": "/vidnet/static/media/flamingos.b8802adc.mov"
+  },
+  {
+    "revision": "29644a029afb431546deaf14bf8f75b8",
+    "url": "/vidnet/static/media/flowers.29644a02.mov"
+  },
+  {
+    "revision": "8de2c46a98f0123099604c07cde821ba",
+    "url": "/vidnet/static/media/flying.8de2c46a.mov"
+  },
+  {
+    "revision": "0a67918ddddad202afc4a502a00221d1",
+    "url": "/vidnet/static/media/grandmas-and-guitars.0a67918d.mov"
+  },
+  {
+    "revision": "80c02a30f0b1e20923c01693af27479d",
+    "url": "/vidnet/static/media/greenday1.80c02a30.mov"
+  },
+  {
+    "revision": "fea159a108ffc4c8e88da6f56c436c00",
+    "url": "/vidnet/static/media/greenday2.fea159a1.mov"
+  },
+  {
+    "revision": "aee52f468abb5a08a15bc936ce8bc76e",
+    "url": "/vidnet/static/media/greenday3.aee52f46.mp4"
+  },
+  {
+    "revision": "6a5e4d62973a7bdfe27668320daa113a",
+    "url": "/vidnet/static/media/halfpipe-wipeout.6a5e4d62.mov"
+  },
+  {
+    "revision": "2abcffbbf1016daa8daad6f9689fd7e4",
+    "url": "/vidnet/static/media/hang-loose.2abcffbb.mov"
+  },
+  {
+    "revision": "57746d645150297379c709163d03c654",
+    "url": "/vidnet/static/media/hockey-women.57746d64.mov"
+  },
+  {
+    "revision": "bffac7ffcfce4ae54bd02f66d467ec8d",
+    "url": "/vidnet/static/media/horse.bffac7ff.mov"
+  },
+  {
+    "revision": "6c4551fbfb94a43e6cace79325a96a72",
+    "url": "/vidnet/static/media/ice-rink.6c4551fb.mov"
+  },
+  {
+    "revision": "fb0b2ebd6a3ed2dbaeba66971593e068",
+    "url": "/vidnet/static/media/juggling.fb0b2ebd.mov"
+  },
+  {
+    "revision": "4d927374eb8128fd1e893b1daf0170f3",
+    "url": "/vidnet/static/media/karate-kids.4d927374.mov"
+  },
+  {
+    "revision": "33a59e99b28edf8049190e60caca71d5",
+    "url": "/vidnet/static/media/kids-street-convo.33a59e99.mov"
+  },
+  {
+    "revision": "c65a7da58765836d75e3feb39659f947",
+    "url": "/vidnet/static/media/kissing-mascott.c65a7da5.mov"
+  },
+  {
+    "revision": "2005414d94dae3daec76a118b9f23c42",
+    "url": "/vidnet/static/media/ladder.2005414d.mov"
+  },
+  {
+    "revision": "db54caccafa73fd10f45618adc135acc",
+    "url": "/vidnet/static/media/lantinx-protest.db54cacc.mov"
+  },
+  {
+    "revision": "b2fa8463f6e4d78ac8df2921fa0f38ce",
+    "url": "/vidnet/static/media/library.b2fa8463.mov"
+  },
+  {
+    "revision": "695ad8ab2338261d340919f2c6433c19",
+    "url": "/vidnet/static/media/mammal.695ad8ab.mov"
+  },
+  {
+    "revision": "d786887ca8c04af70210790f710b67ba",
+    "url": "/vidnet/static/media/night.d786887c.mov"
+  },
+  {
+    "revision": "a6b01396657c6a12814b1aab35a8c9cb",
+    "url": "/vidnet/static/media/ninja.a6b01396.mov"
+  },
+  {
+    "revision": "984f1b19a08eac8d9cb281d98eaae5e9",
+    "url": "/vidnet/static/media/otter.984f1b19.mov"
+  },
+  {
+    "revision": "b9602993607fc5e7531b22dbfdb68381",
+    "url": "/vidnet/static/media/pizza-face.b9602993.mov"
+  },
+  {
+    "revision": "2ab888981d69807a6c3836c375b60b03",
+    "url": "/vidnet/static/media/playground-kids-run.2ab88898.mov"
+  },
+  {
+    "revision": "263f79ab19fe3e063d1b8b56699ef239",
+    "url": "/vidnet/static/media/playground-monkey-bars.263f79ab.mov"
+  },
+  {
+    "revision": "1b8587f4d7a702c65fc9a0aa5a150034",
+    "url": "/vidnet/static/media/playground1.1b8587f4.mov"
+  },
+  {
+    "revision": "0efdbc428f12299b30f0ab6bf1bc686b",
+    "url": "/vidnet/static/media/pool.0efdbc42.mov"
+  },
+  {
+    "revision": "ca2d0ccbbe96ed29623749d04cfd8e7f",
+    "url": "/vidnet/static/media/pyramus_and_thisby.ca2d0ccb.mov"
+  },
+  {
+    "revision": "9181290b38609a476c9b85673ffafec3",
+    "url": "/vidnet/static/media/ride.9181290b.mov"
+  },
+  {
+    "revision": "34b50a58fca09acab5d2d7637dad7374",
+    "url": "/vidnet/static/media/sandcastle.34b50a58.mov"
+  },
+  {
+    "revision": "a502c62cd2471f2e69a50c8c0baf372a",
+    "url": "/vidnet/static/media/school-bus.a502c62c.mov"
+  },
+  {
+    "revision": "f55345ad2fe60e7ccf11f25989887041",
+    "url": "/vidnet/static/media/schoolbus2.f55345ad.mov"
+  },
+  {
+    "revision": "781de5cf0ba9e513b0573906dc8bc165",
+    "url": "/vidnet/static/media/seagulls.781de5cf.mov"
+  },
+  {
+    "revision": "a1943aa0621ca37472af84dcbee67b28",
+    "url": "/vidnet/static/media/show.a1943aa0.mov"
+  },
+  {
+    "revision": "0d527b909aefa8f8bf103076833aedc0",
+    "url": "/vidnet/static/media/ski-tricks.0d527b90.mov"
+  },
+  {
+    "revision": "f7d3d58a2ca2434a0af897591ef6ff2f",
+    "url": "/vidnet/static/media/snowball-ben.f7d3d58a.mov"
+  },
+  {
+    "revision": "3b8c37f6705b31b58444007214daa526",
+    "url": "/vidnet/static/media/snowjob.3b8c37f6.mov"
+  },
+  {
+    "revision": "a9b8f0624e61e7576891310a41ac811d",
+    "url": "/vidnet/static/media/stormy.a9b8f062.mov"
+  },
+  {
+    "revision": "15064e463ea41bb36b9964a6344bbdd1",
+    "url": "/vidnet/static/media/sunny.15064e46.mov"
+  },
+  {
+    "revision": "267f2ffc16e7fade0b8f170d51092d22",
+    "url": "/vidnet/static/media/swim.267f2ffc.mov"
+  },
+  {
+    "revision": "0cdeaf2d230f0eaa762e2ed28ab428c4",
+    "url": "/vidnet/static/media/swimming.0cdeaf2d.mov"
+  },
+  {
+    "revision": "d4e99ca62bcc78fff440e3f68233c23d",
+    "url": "/vidnet/static/media/swing.d4e99ca6.mov"
+  },
+  {
+    "revision": "f597465b18f4c3661b68b292a134a640",
+    "url": "/vidnet/static/media/toy-train.f597465b.mov"
+  },
+  {
+    "revision": "aea9f75343c906f6c989fb1dc98c1bef",
+    "url": "/vidnet/static/media/tsunami1.aea9f753.mov"
+  },
+  {
+    "revision": "d4de2275d1eed5465a76fbfae09b3636",
+    "url": "/vidnet/static/media/tsunami_patong_beach.d4de2275.mov"
+  },
+  {
+    "revision": "fdf6cd70637782dbc35281490eefd3b4",
+    "url": "/vidnet/static/media/tunnel.fdf6cd70.mov"
+  },
+  {
+    "revision": "33fb1ded70e7597b8db556dfce3fcd1b",
+    "url": "/vidnet/static/media/turntables.33fb1ded.mov"
+  },
+  {
+    "revision": "679f3f2f918d22239d7685a67afc0a0b",
+    "url": "/vidnet/static/media/water.679f3f2f.mov"
+  },
+  {
+    "revision": "cc20b191c2ea7029d5855847ffaf576c",
+    "url": "/vidnet/static/media/wrestling-long.cc20b191.mp4"
+  },
+  {
+    "revision": "99b55f6251d2f08002f706880c081843",
+    "url": "/vidnet/static/media/wrestling1.99b55f62.mov"
+  },
+  {
+    "revision": "53a50d84e9ac2ba62a47369b92703275",
+    "url": "/vidnet/static/media/wrestling2.53a50d84.mov"
+  },
+  {
+    "revision": "9be2025e96b928919f1172b62d71b006",
+    "url": "/vidnet/static/media/wrestling3.9be2025e.mov"
+  },
+  {
+    "revision": "a58d4d599bd074c5c3093b7056f95dc8",
+    "url": "/vidnet/static/media/yt-airhorn.a58d4d59.mov"
+  },
+  {
+    "revision": "9c586acbf7dde27969a160caa0d98dd1",
+    "url": "/vidnet/static/media/yt-apple-store.9c586acb.mov"
+  },
+  {
+    "revision": "1aadd44aecdd4cef3b160b629742e839",
+    "url": "/vidnet/static/media/yt-boat.1aadd44a.mov"
+  },
+  {
+    "revision": "54406bf240a8149688e986491795dfb1",
+    "url": "/vidnet/static/media/yt-chase-brown.54406bf2.mov"
+  },
+  {
+    "revision": "c3345fdb4dc51bd56cd05ca8b93610ca",
+    "url": "/vidnet/static/media/yt-chicken-choke.c3345fdb.mov"
+  },
+  {
+    "revision": "051a5d2b6037d87b596fd960872183db",
+    "url": "/vidnet/static/media/yt-cooking-fire.051a5d2b.mov"
+  },
+  {
+    "revision": "7fe1ee13171867c2823cc4532a329e77",
+    "url": "/vidnet/static/media/yt-daily-life.7fe1ee13.mov"
+  },
+  {
+    "revision": "8ec24c247b9ff21587be460d09992d9d",
+    "url": "/vidnet/static/media/yt-guy-lighter.8ec24c24.mov"
+  },
+  {
+    "revision": "0f1641febe56b6c1e62b7c245cded667",
+    "url": "/vidnet/static/media/yt-leave-cab.0f1641fe.mov"
+  },
+  {
+    "revision": "753e84b914e6e6682e0d2abb721fe966",
+    "url": "/vidnet/static/media/yt-mall-ride.753e84b9.mov"
+  },
+  {
+    "revision": "0780f6061bb820356d888a555796f31d",
+    "url": "/vidnet/static/media/yt-pandas.0780f606.mov"
+  },
+  {
+    "revision": "bf218dec65f5426cc3c835d68fa9b08d",
+    "url": "/vidnet/static/media/yt-robot-dance.bf218dec.mov"
+  },
+  {
+    "revision": "0fb4722138380526d603720814efa604",
+    "url": "/vidnet/static/media/yt-rockets-sing.0fb47221.mov"
+  },
+  {
+    "revision": "db7ecb398ff84729a8f7b6463dc9c9db",
+    "url": "/vidnet/static/media/yt-sledding.db7ecb39.mov"
+  },
+  {
+    "revision": "806ca44ac2ae1d99006b0f5de3a5195b",
+    "url": "/vidnet/static/media/yt-smacks-ass.806ca44a.mov"
+  },
+  {
+    "revision": "ec53b8252699a6fd39271a42ca1fc9b9",
+    "url": "/vidnet/static/media/yt-snowboard.ec53b825.mov"
+  },
+  {
+    "revision": "d3543286b49b9c1da5afdcd62f294762",
+    "url": "/vidnet/static/media/yt-uk-drunk-dancing.d3543286.mov"
+  },
+  {
+    "revision": "056b23f340d214660c403f445f988213",
+    "url": "/vidnet/static/media/yt-wall-star.056b23f3.mov"
+  },
+  {
+    "revision": "195fd5ae0f260ba6282e8c0a70d05e45",
+    "url": "/vidnet/static/media/yt-zoo.195fd5ae.mov"
+  },
+  {
+    "revision": "655da73a503206f0a992a4d74f57942a",
+    "url": "/vidnet/static/media/zamboni.655da73a.mov"
+  },
+  {
+    "revision": "58c519867146123facebaac1e215f23e",
+    "url": "/vidnet/static/media/zoo.58c51986.mov"
+  }
+]);
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const applicationStates = { "start", "end", "display" };
